50884e1f58d853955e0949b9fe45e8e7d2322024
Tymeckia Kendall <tykend@gmail.com>
2019-03-08 01:17:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -6,16 +6,9 @@ def hopper
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
 
+        }
+     programmer_hash[:grace_hopper]
 
 end
 
